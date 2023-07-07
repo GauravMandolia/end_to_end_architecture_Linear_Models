35903f78472a0882d69eea20d35fd954bbda35d5
GauravMandolia <g.k.mandolia@gmail.com>
2023-07-07 12:53:45 +0530

    final running website changes

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
+import json
 import pickle
-import os
-from flask import Flask, request, jsonify, render_template
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app = Flask(__name__)
-
-# Load Model
-regmodel = pickle.load(open('regmodel.pkl', 'rb'))
-scaler = pickle.load(open('scaling.pkl', 'rb'))
-
-
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template('home.html')  # First Page of HTML...
+    return render_template('home.html')
 
-
-@app.route('/predict', methods=['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
-    data = request.get_json()  # Use get_json() instead of json()
+    data=request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    output = regmodel.predict(new_data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
     print(output[0])
-    return jsonify(output[0])  # Output is 2-dimensional, so take the first element only.
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     app.run(debug=True)
+   
+     
